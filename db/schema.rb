32d299249b116147801d4b655ba7fa63e5dc31ea
Chris Terlegas <c.terlegas@gmail.com>
2023-06-03 18:39:22 +0100

    created bets model and tags model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_03_153109) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_03_173040) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bets", force: :cascade do |t|
+    t.string "name"
+    t.string "stake"
+    t.string "location"
+    t.date "end_time"
+    t.string "status", default: "pending"
+    t.string "outcome", default: "ongoing"
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_bets_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +41,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_03_153109) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bets", "tags"
 end
