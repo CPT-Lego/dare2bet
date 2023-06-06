4fa996d90096906d6d65c6509cadb5fec936cbda
Dennis WoldMcGimsey <denniswoldmcg@gmail.com>
2023-06-06 01:15:58 +0100

    created show page for bets, nothing really added in it yet

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -2,4 +2,12 @@ class BetsController < ApplicationController
   def index
     @bets = Bet.all
   end
+
+
+  def destroy
+    @bet = Bet.find(params[:id])
+    @bet.destroy
+    redirect_to bets_path(@bet), notice: "Bet was successfully cancelled."
+  end
+
 end
