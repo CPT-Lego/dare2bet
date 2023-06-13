642fe3e1173db3a9e81e47183ce7dc1e88684dbc
Chris Terlegas <c.terlegas@gmail.com>
2023-06-13 13:01:26 +0100

    cloudinary almost done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bet_members
   has_many :bets, through: :bet_members
+  has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
