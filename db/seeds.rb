afdd96080481491f3d25550f01fcc7fe9194c8d5
Mateo Larbodiere <mateo.larbodiere@edhec.com>
2023-06-13 19:21:41 +0200

    show page look a little better

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,13 @@ Bet.destroy_all
 User.destroy_all
 
 puts "Creating users..."
-user_1 = User.new(email: "chris@myemail.com", password: "123123", name: "Chris", location: "Athens, Greece", photo: "https://cdna.artstation.com/p/assets/images/images/015/425/426/large/frederic-caeyers-athenianv4.jpg")
-user_1.save!
-user_2 = User.new(email: "dennis@myemail.com", password: "123123", name: "Dennis", location: "Denver, Colorado, USA", photo: "https://c8.alamy.com/comp/EWEA8G/dennis-the-menace-EWEA8G.jpg")
+user_1 = User.new(email: "chris@myemail.com", password: "123123", name: "Chris", location: "Athens, Greece")
+user_2 = User.new(email: "dennis@myemail.com", password: "123123", name: "Dennis", location: "Denver, Colorado, USA")
 user_2.save!
-user_3 = User.new(email: "mateo@myemail.com", password: "123123", name: "Mateo", location: "Paris, France", photo: "https://media.istockphoto.com/id/157421404/pt/foto/franc%C3%AAs-baguettes-e-vinho.jpg?s=1024x1024&w=is&k=20&c=81ACw2BfCvcQ1bZa_q2Lpi9NxnseQDuxLpzWIblpofc=")
-user_3.save!
-user_4 = User.new(email: "miguel@myemail.com", password: "123123", name: "Miguel", location: "Lisbon, Portugal", photo: "https://cdn.resfu.com/media/img_news/goal_nuno-gomes_llwaw6jb7c5h1dus80yol8krn.jpg")
+user_3 = User.new(email: "mateo@myemail.com", password: "123123", name: "Mateo", location: "Paris, France")
+user_4 = User.new(email: "miguel@myemail.com", password: "123123", name: "Miguel", location: "Lisbon, Portugal")
 user_4.save!
-user_5 = User.new(email: "riri@myemail.com", password: "123123", name: "Rihanna", location: "Bridgetown, Barbados", photo: "https://static.wikia.nocookie.net/whumpapedia/images/0/07/Rihanna.jpeg")
+user_5 = User.new(email: "riri@myemail.com", password: "123123", name: "Rihanna", location: "Bridgetown, Barbados")
 user_5.save!
 
 tag_1 = Tag.new(name: "Sport")
